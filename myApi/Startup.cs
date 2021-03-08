a3602c8465e277def1bb0ff3466be4259f46785e
Chris Woosley <woosleyc@vystarcu.org>
2021-03-08 09:39:47 -0500

    update

--- myApi/Startup.cs
+++ myApi/Startup.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using AutoMapper.Configuration;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using myApi.Services;
 using System;
 using System.Collections.Generic;
@@ -20,19 +22,31 @@ namespace myApi
         //    _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         //}
 
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLogging();
+
             services.AddMvc()
                 .AddMvcOptions(o =>
                 {
                     o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
                 });
 
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new Info
+            //    {
+            //        Version = "v1",
+            //        Title = "myDemo.API",
+            //        Description = ".Net Core Web API"
+            //    });
+            //});
 
-            // services.AddScoped<IPersonRepository, PersonRepository>();
+            //services.AddScoped<IPersonRepository, PersonRepository>();
 
             //services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
@@ -53,6 +67,13 @@ namespace myApi
             app.UseStatusCodePages();
 
             app.UseMvc();
+
+            //app.UseSwagger();
+
+            //app.UseSwaggerUI(c =>
+            //{
+            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyDemoAPI V1");
+            //});
         }
     }
 }
